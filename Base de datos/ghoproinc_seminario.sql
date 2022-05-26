7a90a4d33a95f119f8951ebc33748a545124e928
gstolosa <gtolosa841@gmail.com>
2022-05-26 18:42:53 -0300

    Update database

--- Base de datos/ghoproinc_seminario.sql
+++ Base de datos/ghoproinc_seminario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-11-2021 a las 18:42:49
+-- Tiempo de generación: 26-05-2022 a las 23:41:31
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -37,18 +37,6 @@ CREATE TABLE `proveedor` (
   `telefono` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `proveedor`
---
-
-INSERT INTO `proveedor` (`id`, `razon_social`, `nombre`, `cuit`, `direccion`, `ciudad`, `telefono`, `email`) VALUES
-(3, 'HardEs SRL', 'Gabriel', '22-222222-2', 'Cuenca 1234', 'CABA', '1234-4567', 'test@test.com'),
-(4, 'Componentes SRL', 'Lucas', '33-333333-3', 'Av. Gaona 123', 'CABA', '7891-1234', 'mail@lucas.com'),
-(5, 'ProPC SRL', 'Gisela', '44-444444-4', 'Helguera 2589', 'CABA', '3698-2587', 'gisela@test.com'),
-(8, 'PCtest SRL', 'Juan', '55-222222-4', 'Calle 15', 'La Plata', '221221221', 'test@mail.com'),
-(9, 'Emprendimiento SRL', 'Santino', '22-777777-1', 'Entre Ríos 258', 'Rosario', '34158962147', 'san@san.com'),
-(10, 'Empresa SRL', 'Apolo', '66-555555-8', 'Rojas 123', 'Bahía Blanca', '2915896345', 'apolo@empresa.com');
 
 --
 -- Índices para tablas volcadas
